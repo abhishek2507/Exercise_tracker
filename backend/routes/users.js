93adb63d8649cb959bad398a1c99061786816d7f
Abhishek <2507abhishek@gmail.com>
2019-08-25 21:55:58 +0530

    completed exercise routing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 router.route('/').get((req,res) => {
-    User.find()
-    .then(user => res.json(users))
+    User.find()     //This is a mongoose method for mongo DB to display all elememts
+    .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
