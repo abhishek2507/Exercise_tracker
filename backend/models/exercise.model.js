89648cc1fa928a69c63b99fb7f70e1a29e2dad71
Abhishek <abhishek2507@gmail.com>
2019-08-25 18:51:38 +0530

    Initialised the models basic api and th erver setup for MERN Stack

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,16 @@
+import { Schema } from "mongoose";
+
+const exerciseSchema = new Schema({
+    username: { type: String, required: true},
+    description: { type: String, required: true},
+    duration: { type: Number, required: true},
+    date: { type: Date, required: true},
+}, {
+    timestamps: true,
+});
+
+
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
