f5ddef545ed3c53192c0a3a9a0fba08ef2931970
Abhishek <2507abhishek@gmail.com>
2019-08-26 19:04:22 +0530

    React components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component.js";
+import ExerciseList from "./components/exercise-list.component.js";
+import EditExercise from "./components/edit-exercise.component.js";
+import CreateUser from "./components/create-user.component.js";
+import CreateExercise from "./components/create-exercise.component.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <br/>
+      <Route path="/" exact component = {ExerciseList} />
+      <Route path="/edit/:id" exact component = {EditExercise} />
+      <Route path="/create" exact component = {CreateExercise} />
+      <Route path="/user" exact component = {CreateUser} />
+    </Router>
   );
 }
 
